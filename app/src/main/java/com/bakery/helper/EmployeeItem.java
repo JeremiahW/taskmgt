8a52000dafa88a5a9ebaa96fb9bb1caa8ebe9f89
wangjixiao@gmail.com <wangjixiao@gmail.com>
2015-10-18 13:02:16 +0800

    Init Add Files 2

---
+++ app/src/main/java/com/bakery/helper/EmployeeItem.java
@@ -0,0 +1,14 @@
+package com.bakery.helper;
+
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by wangj on 10/18/15.
+ */
+public final class EmployeeItem {
+    public ImageButton BtnDetails;
+    public TextView TxtEmpName;
+    public TextView TxtEmpPhone;
+    public TextView TxtEmpId;
+}
